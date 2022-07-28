ab030858e265f9692cdaf9c639f02be9aecd918f
tayefulislam <kazitayefulislam12@gmail.com>
2022-07-28 22:04:09 +0600

    Pages And Components Folders added

---
+++ src/Components/Test/Test.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Test = () => {
+  return (
+    <div>
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload. Test
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default Test;

7eabb265472c400661ef8160a70bfe7dac2e6b55
tayefulislam <kazitayefulislam12@gmail.com>
2022-07-28 22:08:59 +0600

    Test component and test.css added

--- src/Components/Test/Test.js
+++ src/Components/Test/Test.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Test.css";
 
 const Test = () => {
   return (
--- src/Pages/Test/Test2.js
+++ src/Pages/Test/Test2.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Test2.css";
 
 const Test = () => {
   return (
